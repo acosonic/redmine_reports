009f2fa26124b92487e1ffd0fa00d67f56057970
Aleksandar Pavic <acosonic@gmail.com>
2017-01-04 15:07:46 +0100

    English translation and alternating rows

--- init.rb
+++ init.rb
@@ -9,7 +9,11 @@ Redmine::Plugin.register :redmine_reports do
   menu :top_menu,
     :redmine_reports_link,
     { :controller => 'redmine_reports', :action => 'index'},
-    :caption => 'report',
+    :caption => 'Custom SQL reports',
     :last => true
-
+  menu :top_menu,
+    :redmine_new_reports_link,
+    { :controller => 'redmine_reports', :action => 'new'},
+    :caption => 'New SQL Report',
+    :last => true
 end
